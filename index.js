97fde94921c4aac680bfaf9dd23d7ec76ffec83c
lwr <ran0046@foxmail.com>
2017-04-19 00:17:35 +0800

    update

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const fs = require('fs'),
       path = require('path');
 
-//module.exports = function (req, res) {
-    let data = {
-        files: []
-    };
+// module.exports = function(){
+    let dirs = [];
+    let root = path.resolve(__dirname, 'demos');
 
-    let dir, root, html, title;
-    root = path.resolve(__dirname, 'demos');
-    if (fs.existsSync(root)) {
-        fs.readdirSync(__dirname+'/demos', function(err, files){
-            if(err)
-                console.log(err)
+    if(fs.existsSync(root)){
+        fs.readdirSync(root).forEach( dir => {
 
-            console.log(files)
-        });
+            let o = {};
+            o.flag = '&clubs;';
+            o.name = dir;
+            o.files = [];
 
+            let title, src;
 
-        /*fs.readdirSync(root).files.forEach(function(file) {
-            console.log(file)
-            fs.stat(root+'/'+file, function(err, stat){
-                if(stat.isDirectory()){
-                    console.log(file)
+            fs.readdirSync(path.join(root, dir)).forEach( file => {
+                src = path.join(root, dir, file);
 
+                if(fs.statSync(src).isDirectory()){
+                    src = path.join(src, 'index.html');
                 }
+                title = fs.readFileSync(src, 'utf8').match(/<title>([^<]+?)<\/title>/);
+                title = title ? title[1] : '';
+                src = path.relative(__dirname , src);
+
+                o.files.push({
+                    flag: '&bull;',
+                    title: title,
+                    src: src
+                });
             });
 
-            html = fs.readFileSync(file, 'utf8');
-            title = html.match(/<title>([^<]+?)<\/title>/);
-            title = title ? title[1] : '';
-
-            file = path.relative(root, file);
-            data.files.push({
-                 flag: '&hearts;',
-                 href: resolve('/src/' + file),
-                 path: resolve('src/' + file),
-                 title: title
-            })
-
-
-        });*/
+            dirs.push(o);
+        });
     }
-//}
+
+    let html = '', htmlIn;
+
+    dirs.forEach( i => {
+
+        htmlIn = '';
+        i.files.forEach( v => {
+            htmlIn += '\n'+'  <dd>'+ v.flag +' <a href="'+ v.src +'">'+ v.src +' - '+ v.title +'</a></dd>';
+        });
+
+        html += '\n'+'<dl>'+'\n'+'  <dt>'+ i.flag +' <b>'+ i.name +'</b></dt>'+ htmlIn +''+'\n'+'</dl>';
+    });
+    html = '<body>'+ html +'\n'+'</body>';
+    html = fs.readFileSync(__dirname + '/temp.html', 'utf8').replace(/<body><\/body>/, html);
+
+    fs.writeFileSync(__dirname + '/index.html', html);
+    console.log('**Demos list saved in index.html.**');
+// };
--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 const express = require('express'),
       app = express();
+      indexJs = require('./index');
 
 const default_port = 9901;
 
+app.use(indexJs);
 app.use('/', express.static('./', { redirect:false }));
-
-//app.use('*', require('./index'));
 
 const index = process.argv.indexOf('--port'),
       port = index > -1 ? (process.argv[index + 1] || default_port) : default_port;
